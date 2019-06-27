e1bdefe7ddbe75c6d406843394d7ec0bd1878a85
John Williams <jrw@mit.edu>
2019-06-27 10:48:54 -0400

    earthling change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.get('/', function(req, res) {
-    res.send('<h1> Hello World </h1>');
+    res.send('<h1> Hello Earthling </h1>');
 });
 
 app.listen(3000);
