898345aeea83acf24defb8b8699e3e6be8c226b4
John Williams <jrw@mit.edu>
2019-08-29 10:21:15 -0400

    routes1

---
+++ server0.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get('/', function(req, res) {
+    res.send('<h1> Hello World </h1>');
+});
+app.get('/f1', function(req, res) {
+    res.send('<body bgcolor= "red">');
+});
+app.get('/f2', function(req, res) {
+    res.send('<body bgcolor= "green">');
+});
+
+
+app.listen(3000);
+console.log("Running on port 3000");
